b263241399fb85d29349c9eabe50e1de071d16bd
Nicolai Waniek <nicolai.waniek@sphere71.com>
2018-04-23 14:01:34 +0200

    generate more figures

--- generate_figures.sh
+++ generate_figures.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
+# set the seed to get real consecutive images
+
 # this script runs several simulations
-./demo.py --hide-final-plot --Tmax 50000 square output/square_50000.svg
-./demo.py --hide-final-plot --Tmax 50000 circular output/circular_50000.svg
-./demo.py --hide-final-plot --Tmax 50000 triangular output/triangular_50000.svg
+./demo.py --hide-final-plot --seed 1337 --Tmax 1000 square output/square_01000.svg
+./demo.py --hide-final-plot --seed 1337 --Tmax 5000 square output/square_05000.svg
+./demo.py --hide-final-plot --seed 1337 --Tmax 10000 square output/square_10000.svg
+./demo.py --hide-final-plot --seed 1337 --Tmax 20000 square output/square_20000.svg
+
+./demo.py --hide-final-plot --seed 1337 --Tmax 1000 circular output/circular_01000.svg
+./demo.py --hide-final-plot --seed 1337 --Tmax 5000 circular output/circular_05000.svg
+./demo.py --hide-final-plot --seed 1337 --Tmax 10000 circular output/circular_10000.svg
+./demo.py --hide-final-plot --seed 1337 --Tmax 20000 circular output/circular_20000.svg
+
+./demo.py --hide-final-plot --seed 1337 --Tmax 1000 triangular output/triangular_01000.svg
+./demo.py --hide-final-plot --seed 1337 --Tmax 5000 triangular output/triangular_05000.svg
+./demo.py --hide-final-plot --seed 1337 --Tmax 10000 triangular output/triangular_10000.svg
+./demo.py --hide-final-plot --seed 1337 --Tmax 20000 triangular output/triangular_20000.svg
 
 # get some consecutive images for TMaze
-# set the seed to get real consecutive images
-./demo.py --hide-final-plot --seed 1337 --Tmax 1000 tmaze output/tmaze_1000.svg
-./demo.py --hide-final-plot --seed 1337 --Tmax 20000 tmaze output/tmaze_20000.svg
-./demo.py --hide-final-plot --seed 1337 --Tmax 100000 tmaze output/tmaze_100000.svg
+#./demo.py --hide-final-plot --seed 1337 --Tmax 1000 tmaze output/tmaze_1000.svg
+./demo.py --hide-final-plot --seed 1337 --Tmax 5000 tmaze output/tmaze_5000.svg
+./demo.py --hide-final-plot --seed 1337 --Tmax 10000 tmaze output/tmaze_10000.svg
+#./demo.py --hide-final-plot --seed 1337 --Tmax 20000 tmaze output/tmaze_20000.svg
